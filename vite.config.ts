1450fa5ca8b3248d40f7665a97869c1c2c656f94
Vladimir Antonio F. C. <50648871+vladiantio@users.noreply.github.com>
2025-03-25 03:33:50 -0300

    chore: add `vite-plugin-svgr` for SVG imports and update configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,6 +11,7 @@ export default defineConfig({
     TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
     react(),
     tailwindcss(),
+    svgr(),
   ],
   resolve: {
     alias: {
