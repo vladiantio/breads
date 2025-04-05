08fad6b85cd6a0fc15aa9a39b39199b4d265d2e3
Vladimir Antonio F. C. <50648871+vladiantio@users.noreply.github.com>
2025-04-05 02:11:22 -0300

    style: enhance tab styles for improved spacing and visual consistency

--- src/components/feed/FeedTabs.tsx
+++ src/components/feed/FeedTabs.tsx
@@ -8,16 +8,16 @@ const FeedTabs: FC = () => {
   return (
     <Tabs className="sticky bg-background top-0 z-10" value={activeTab} onValueChange={setActiveTab}>
       <ScrollArea>
-        <TabsList className="gap-2 rounded-none bg-transparent p-2 min-w-full">
+        <TabsList className="rounded-none bg-transparent p-3 min-w-full">
           <TabsTrigger
             value="discover"
-            className="grow data-[state=active]:after:bg-primary relative after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 after:rounded-full data-[state=active]:bg-transparent data-[state=active]:shadow-none hover:bg-accent data-[state=active]:active:bg-accent/60 data-[state=active]:hover:bg-accent"
+            className="grow data-[state=active]:after:bg-primary relative after:absolute after:inset-x-0 after:mx-auto after:max-w-16 after:bottom-0 after:-mb-1 after:h-1 after:rounded-full data-[state=active]:bg-transparent data-[state=active]:shadow-none"
           >
             Discover
           </TabsTrigger>
           <TabsTrigger
             value="following"
-            className="grow data-[state=active]:after:bg-primary relative after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 after:rounded-full data-[state=active]:bg-transparent data-[state=active]:shadow-none hover:bg-accent data-[state=active]:active:bg-accent/60 data-[state=active]:hover:bg-accent"
+            className="grow data-[state=active]:after:bg-primary relative after:absolute after:inset-x-0 after:mx-auto after:max-w-16 after:bottom-0 after:-mb-1 after:h-1 after:rounded-full data-[state=active]:bg-transparent data-[state=active]:shadow-none"
           >
             Following
           </TabsTrigger>
--- src/components/profile/ProfileTabs.tsx
+++ src/components/profile/ProfileTabs.tsx
@@ -8,22 +8,22 @@ export const ProfileTabs: FC = () => {
   return (
     <Tabs className="sticky bg-background top-14 z-10" value={activeTab} onValueChange={setActiveTab}>
       <ScrollArea>
-        <TabsList className="gap-x-2 rounded-none bg-transparent p-2 min-w-full">
+        <TabsList className="rounded-none bg-transparent p-3 min-w-full">
           <TabsTrigger
             value="posts"
-            className="grow data-[state=active]:after:bg-primary relative after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 after:rounded-full data-[state=active]:bg-transparent data-[state=active]:shadow-none hover:bg-accent data-[state=active]:active:bg-accent/60 data-[state=active]:hover:bg-accent"
+            className="grow data-[state=active]:after:bg-primary relative after:absolute after:inset-x-0 after:mx-auto after:max-w-16 after:bottom-0 after:-mb-1 after:h-1 after:rounded-full data-[state=active]:bg-transparent data-[state=active]:shadow-none"
           >
             Posts
           </TabsTrigger>
           <TabsTrigger
             value="replies"
-            className="grow data-[state=active]:after:bg-primary relative after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 after:rounded-full data-[state=active]:bg-transparent data-[state=active]:shadow-none hover:bg-accent data-[state=active]:active:bg-accent/60 data-[state=active]:hover:bg-accent"
+            className="grow data-[state=active]:after:bg-primary relative after:absolute after:inset-x-0 after:mx-auto after:max-w-16 after:bottom-0 after:-mb-1 after:h-1 after:rounded-full data-[state=active]:bg-transparent data-[state=active]:shadow-none"
           >
             Replies
           </TabsTrigger>
           <TabsTrigger
             value="reposts"
-            className="grow data-[state=active]:after:bg-primary relative after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 after:rounded-full data-[state=active]:bg-transparent data-[state=active]:shadow-none hover:bg-accent data-[state=active]:active:bg-accent/60 data-[state=active]:hover:bg-accent"
+            className="grow data-[state=active]:after:bg-primary relative after:absolute after:inset-x-0 after:mx-auto after:max-w-16 after:bottom-0 after:-mb-1 after:h-1 after:rounded-full data-[state=active]:bg-transparent data-[state=active]:shadow-none"
           >
             Reposts
           </TabsTrigger>
