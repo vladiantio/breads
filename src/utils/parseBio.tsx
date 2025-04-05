864cd8017d7039c6094e21276c23b9f1666e67e3
Vladimir Antonio F. C. <50648871+vladiantio@users.noreply.github.com>
2025-04-05 18:08:03 -0300

    feat(parseBio): add support for mentions with AuthorHoverCard integration

--- src/utils/parseBio.tsx
+++ src/utils/parseBio.tsx
@@ -1,8 +1,11 @@
+import { Link } from "@tanstack/react-router";
 import { removeHttpAndWww, truncateString } from "./string";
 import { Fragment } from "react";
+import { AuthorHoverCard } from "@/components/feed/AuthorHoverCard";
 
 // Regular expressions for URLs and email addresses
-const urlRegex = /^(https?:\/\/[^\s]+)|((www\.)?[^\s]+\.[^\s]+)/;
+const mentionRegex = /^(@[\w\d.-]+)/;
+const urlRegex = /^(https?:\/\/[^\s]+)|((www\.)?[^\s]+\.[^\d\s]+\/?[^s]*)/;
 const emailRegex = /^([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/;
 
 const aClassName = "text-link hover:underline active:opacity-60";
@@ -15,7 +18,28 @@ const processLine = (line: string) => {
     const space = wordIndex < words.length - 1 ? ' ' : '';
 
     // Check if the word is a URL
-    if (urlRegex.test(word)) {
+    if (mentionRegex.test(word)) {
+      const handle = word.slice(1);
+      return (
+        <Fragment key={`word-${wordIndex}`}>
+          <AuthorHoverCard handle={handle}>
+            <Link
+              className={aClassName}
+              to="/profile/$username"
+              params={{
+                username: handle
+              }}
+            >
+              {word}
+            </Link>
+          </AuthorHoverCard>
+          {space}
+        </Fragment>
+      );
+    }
+
+    // Check if the word is a URL
+    else if (urlRegex.test(word)) {
       const href = word.startsWith('http') ? word.replace(/^http:/, 'https:') : `https://${word}`;
       return (
         <Fragment key={`word-${wordIndex}`}>
