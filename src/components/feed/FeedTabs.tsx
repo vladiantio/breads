7737529f141710ae99735ed0a84474e5a38618bf
Vladimir Antonio F. C. <50648871+vladiantio@users.noreply.github.com>
2025-04-05 12:11:29 -0300

    refactor(Tabs): simplify tab trigger styles and add underline variant

--- src/components/feed/FeedTabs.tsx
+++ src/components/feed/FeedTabs.tsx
@@ -6,18 +6,23 @@ const FeedTabs: FC = () => {
   const [activeTab, setActiveTab] = useState('discover');
 
   return (
-    <Tabs className="sticky bg-background top-0 z-10" value={activeTab} onValueChange={setActiveTab}>
+    <Tabs
+      className="sticky bg-background top-0 z-10"
+      variant="underline"
+      value={activeTab}
+      onValueChange={setActiveTab}
+    >
       <ScrollArea>
-        <TabsList className="rounded-none bg-transparent p-3 min-w-full">
+        <TabsList className="p-1.5 min-w-full">
           <TabsTrigger
             value="discover"
-            className="grow data-[state=active]:after:bg-primary relative after:absolute after:inset-x-0 after:mx-auto after:max-w-16 after:bottom-0 after:-mb-1 after:h-1 after:rounded-full data-[state=active]:bg-transparent data-[state=active]:shadow-none"
+            className="grow"
           >
             Discover
           </TabsTrigger>
           <TabsTrigger
             value="following"
-            className="grow data-[state=active]:after:bg-primary relative after:absolute after:inset-x-0 after:mx-auto after:max-w-16 after:bottom-0 after:-mb-1 after:h-1 after:rounded-full data-[state=active]:bg-transparent data-[state=active]:shadow-none"
+            className="grow"
           >
             Following
           </TabsTrigger>
--- src/components/profile/ProfileTabs.tsx
+++ src/components/profile/ProfileTabs.tsx
@@ -6,24 +6,29 @@ export const ProfileTabs: FC = () => {
   const [activeTab, setActiveTab] = useState('posts');
 
   return (
-    <Tabs className="sticky bg-background top-14 z-10" value={activeTab} onValueChange={setActiveTab}>
+    <Tabs
+      className="sticky bg-background top-14 z-10"
+      variant="underline"
+      value={activeTab}
+      onValueChange={setActiveTab}
+    >
       <ScrollArea>
-        <TabsList className="rounded-none bg-transparent p-3 min-w-full">
+        <TabsList className="p-1.5 min-w-full">
           <TabsTrigger
             value="posts"
-            className="grow data-[state=active]:after:bg-primary relative after:absolute after:inset-x-0 after:mx-auto after:max-w-16 after:bottom-0 after:-mb-1 after:h-1 after:rounded-full data-[state=active]:bg-transparent data-[state=active]:shadow-none"
+            className="grow"
           >
             Posts
           </TabsTrigger>
           <TabsTrigger
             value="replies"
-            className="grow data-[state=active]:after:bg-primary relative after:absolute after:inset-x-0 after:mx-auto after:max-w-16 after:bottom-0 after:-mb-1 after:h-1 after:rounded-full data-[state=active]:bg-transparent data-[state=active]:shadow-none"
+            className="grow"
           >
             Replies
           </TabsTrigger>
           <TabsTrigger
             value="reposts"
-            className="grow data-[state=active]:after:bg-primary relative after:absolute after:inset-x-0 after:mx-auto after:max-w-16 after:bottom-0 after:-mb-1 after:h-1 after:rounded-full data-[state=active]:bg-transparent data-[state=active]:shadow-none"
+            className="grow"
           >
             Reposts
           </TabsTrigger>
