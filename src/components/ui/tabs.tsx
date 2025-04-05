bf534d12ec58a8f2c093df26ab0287e396718984
Vladimir Antonio F. C. <50648871+vladiantio@users.noreply.github.com>
2025-04-05 12:07:03 -0300

    refactor(Tabs): implement context for variant management and enhance trigger styles

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -2,17 +2,58 @@ import * as React from "react"
 import * as TabsPrimitive from "@radix-ui/react-tabs"
 
 import { cn } from "@/lib/utils"
+import { cva, type VariantProps } from "class-variance-authority"
+
+const tabsTriggerVariants = cva(
+  "hover:text-muted-foreground data-[state=active]:text-foreground focus-visible:border-ring focus-visible:ring-ring/50 inline-flex items-center justify-center rounded-sm px-3 py-2 text-sm font-medium whitespace-nowrap transition-all outline-none focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&_svg]:shrink-0",
+  {
+    variants: {
+      variant: {
+        default:
+          "data-[state=active]:bg-accent data-[state=active]:shadow-xs",
+        underline:
+          "data-[state=active]:after:bg-primary relative after:absolute after:inset-x-0 after:mx-auto after:max-w-16 after:bottom-0 after:h-1 after:rounded-full",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+)
+
+type TabsContextType = VariantProps<typeof tabsTriggerVariants>
+
+const TabsContext = React.createContext<TabsContextType | null>(null)
+
+function useTabs() {
+  const context = React.useContext(TabsContext)
+  if (!context) {
+    throw new Error("useTabs must be used within a Tabs.")
+  }
+
+  return context
+}
 
 function Tabs({
   className,
+  variant,
   ...props
-}: React.ComponentProps<typeof TabsPrimitive.Root>) {
+}: React.ComponentProps<typeof TabsPrimitive.Root> & VariantProps<typeof tabsTriggerVariants>) {
+  const contextValue = React.useMemo<TabsContextType>(
+    () => ({
+      variant,
+    }),
+    [variant]
+  )
+
   return (
-    <TabsPrimitive.Root
-      data-slot="tabs"
-      className={cn("flex flex-col gap-2", className)}
-      {...props}
-    />
+    <TabsContext.Provider value={contextValue}>
+      <TabsPrimitive.Root
+        data-slot="tabs"
+        className={cn("flex flex-col gap-2", className)}
+        {...props}
+      />
+    </TabsContext.Provider>
   )
 }
 
@@ -36,13 +77,12 @@ function TabsTrigger({
   className,
   ...props
 }: React.ComponentProps<typeof TabsPrimitive.Trigger>) {
+  const { variant } = useTabs()
+
   return (
     <TabsPrimitive.Trigger
       data-slot="tabs-trigger"
-      className={cn(
-        "hover:text-muted-foreground data-[state=active]:bg-background data-[state=active]:text-foreground focus-visible:border-ring focus-visible:ring-ring/50 inline-flex items-center justify-center rounded-sm px-3 py-1.5 text-sm font-medium whitespace-nowrap transition-all outline-none focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-xs [&_svg]:shrink-0",
-        className,
-      )}
+      className={cn(tabsTriggerVariants({ variant, className }))}
       {...props}
     />
   )
