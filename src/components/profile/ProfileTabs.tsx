155e003643968ebb7f643e5b6c22d379a7c6b3f1
Vladimir Antonio F. C. <50648871+vladiantio@users.noreply.github.com>
2025-04-05 01:46:01 -0300

    refactor: update tab styles for improved spacing and hover effects

--- src/components/profile/ProfileTabs.tsx
+++ src/components/profile/ProfileTabs.tsx
@@ -8,22 +8,22 @@ export const ProfileTabs: FC = () => {
   return (
     <Tabs className="sticky bg-background top-14 z-10" value={activeTab} onValueChange={setActiveTab}>
       <ScrollArea>
-        <TabsList className="gap-x-1 rounded-none border-b bg-transparent p-1 min-w-full">
+        <TabsList className="gap-x-2 rounded-none bg-transparent p-2 min-w-full">
           <TabsTrigger
             value="posts"
-            className="grow data-[state=active]:after:bg-primary relative after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:-mx-1 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none hover:bg-accent data-[state=active]:hover:bg-accent"
+            className="grow data-[state=active]:after:bg-primary relative after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 after:rounded-full data-[state=active]:bg-transparent data-[state=active]:shadow-none hover:bg-accent data-[state=active]:active:bg-accent/60 data-[state=active]:hover:bg-accent"
           >
             Posts
           </TabsTrigger>
           <TabsTrigger
             value="replies"
-            className="grow data-[state=active]:after:bg-primary relative after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:-mx-1 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none hover:bg-accent data-[state=active]:hover:bg-accent"
+            className="grow data-[state=active]:after:bg-primary relative after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 after:rounded-full data-[state=active]:bg-transparent data-[state=active]:shadow-none hover:bg-accent data-[state=active]:active:bg-accent/60 data-[state=active]:hover:bg-accent"
           >
             Replies
           </TabsTrigger>
           <TabsTrigger
             value="reposts"
-            className="grow data-[state=active]:after:bg-primary relative after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:-mx-1 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none hover:bg-accent data-[state=active]:hover:bg-accent"
+            className="grow data-[state=active]:after:bg-primary relative after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 after:rounded-full data-[state=active]:bg-transparent data-[state=active]:shadow-none hover:bg-accent data-[state=active]:active:bg-accent/60 data-[state=active]:hover:bg-accent"
           >
             Reposts
           </TabsTrigger>
