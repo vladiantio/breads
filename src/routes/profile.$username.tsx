4d991063d07b8562638c6e839a73a9e920004cfb
Vladimir Antonio F. C. <50648871+vladiantio@users.noreply.github.com>
2025-04-07 19:56:16 -0400

    feat(ProfileRoute): implement infinite scrolling for user posts with error handling

--- src/routes/profile.$username.tsx
+++ src/routes/profile.$username.tsx
@@ -1,12 +1,25 @@
 import PostCard from '@/components/feed/PostCard'
 import ProfileHeader from '@/components/profile/ProfileHeader'
-import { getPostWithAuthor, getUserPosts } from '@/data/posts'
-import { getCurrentUser, getUserByUsername } from '@/data/users'
+import { Button } from '@/components/ui/button'
+import { createAgent, getActorProfile, getAuthorFeed, getProfile } from '@/lib/atproto-helpers'
+import { ResponseSchema } from '@/types/ResponseSchema'
+import { InfiniteData, QueryKey, useInfiniteQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
-import { useMemo } from 'react'
+import { ArrowDownIcon } from 'lucide-react'
+
+const agent = createAgent()
+
+const fetchFeed = (did:string, cursor?: string) => {
+  return getAuthorFeed(agent, did, cursor);
+}
 
 export const Route = createFileRoute('/profile/$username')({
-  loader: async ({ params: { username } }) => getUserByUsername(username),
+  loader: ({ params: { username } }) => {
+    if (username.startsWith('did:'))
+      return getActorProfile(agent, username);
+    else
+      return getProfile(agent, username);
+  },
   notFoundComponent: () => {
     return <div className="feed-container pt-16 text-center">
       <h1 className="text-2xl font-bold mb-4">User not found</h1>
@@ -19,22 +32,57 @@ export const Route = createFileRoute('/profile/$username')({
 function RouteComponent() {
   const user = Route.useLoaderData()
 
-  const userPosts = useMemo(() => {
-    if (!user) return [];
-    
-    const posts = getUserPosts(user.id);
-    return posts.map(post => getPostWithAuthor(post.id)!);
-  }, [user]);
+  const {
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+  } = useInfiniteQuery<ResponseSchema, Error, InfiniteData<ResponseSchema>, QueryKey, string | undefined>({
+    queryKey: ['feedData'],
+    queryFn: ({ pageParam }) => fetchFeed(user.id, pageParam),
+    initialPageParam: undefined,
+    getNextPageParam: (lastPage) => lastPage.cursor,
+  });
   
-  const isCurrentUser = user?.id === getCurrentUser().id;
+  const isCurrentUser = false;
 
   if (!user)
     return "Nothing to show!";
 
   return <>
     <ProfileHeader user={user} isCurrentUser={isCurrentUser} />
+
+    {error ? 'An error has occurred: ' + error.message : null}
+
+    {data?.pages.map((page, pageIndex) =>
+      page.posts.map((post, postIndex) => (
+        <PostCard
+          key={`${pageIndex}-${postIndex}`}
+          post={post}
+          fromATP
+          isPinned={user.pinnedPost?.cid === post.id}
+        />
+      )
+    ))}
+    {isFetching ? (
+      <span>Loading...</span>
+    ) : (
+      <div className="pb-3 text-center">
+        <Button
+          variant="outline"
+          onClick={() => fetchNextPage()}
+          disabled={!hasNextPage}
+        >
+          {hasNextPage
+            ? 'Load more'
+            : 'Nothing more to load'}
+          {hasNextPage && <ArrowDownIcon />}
+        </Button>
+      </div>
+    )}
     
-    {userPosts.length > 0 ? (
+    {/* {userPosts.length > 0 ? (
       <div>
         {userPosts.map(post => (
           <PostCard key={post.id} post={post} />
@@ -48,6 +96,6 @@ function RouteComponent() {
           they'll appear here
         </p>
       </div>
-    )}
+    )} */}
   </>
 }
