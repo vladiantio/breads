f63c1951e80dc30a5371a2ea4c71f5a235205abf
Vladimir Antonio F. C. <50648871+vladiantio@users.noreply.github.com>
2025-04-08 04:16:36 -0400

    feat(PostCardContent): integrate EmbedToggle and EmbedExternal components for improved media embedding

--- src/components/feed/PostCardContent.tsx
+++ src/components/feed/PostCardContent.tsx
@@ -4,19 +4,11 @@ import {
   AppBskyEmbedVideo,
   Facet
 } from '@atproto/api';
-import {
-  EyeIcon,
-  EyeOffIcon,
-  GlobeIcon
-} from 'lucide-react';
 import HLSPlayer from '../shared/HLSPlayer';
 import { RichTextRenderer } from '../shared/RichTextRenderer';
 import { ThreadContentRenderer } from '../shared/ThreadContentRenderer';
-import { Button } from '../ui/button';
-import YouTubeEmbed from '../shared/YouTubeEmbed';
-
-const gifUriRegex = /\.gif(\?[\w\d=&-]*)?$/;
-const ytUriRegex = /(?:(?:youtu.be\/)|(?:v\/)|(?:\/u\/\w\/)|(?:embed\/)|(?:watch\?))\??v?=?([^#&?]*)/;
+import { EmbedExternal } from './EmbedExternal';
+import { EmbedToggle } from './EmbedToggle';
 
 interface PostCardContentProps {
   fromATP: boolean;
@@ -25,8 +17,6 @@ interface PostCardContentProps {
   embedImages?: AppBskyEmbedImages.ViewImage[];
   embedVideo?: AppBskyEmbedVideo.View;
   embedExternal?: AppBskyEmbedExternal.ViewExternal;
-  showEmbed: boolean;
-  onEmbedToggle: (e: React.MouseEvent) => void;
 }
 
 const PostCardContent: React.FC<PostCardContentProps> = ({
@@ -36,8 +26,6 @@ const PostCardContent: React.FC<PostCardContentProps> = ({
   embedImages,
   embedVideo,
   embedExternal,
-  showEmbed,
-  onEmbedToggle,
 }) => {
   return (
     <>
@@ -47,142 +35,54 @@ const PostCardContent: React.FC<PostCardContentProps> = ({
           : <ThreadContentRenderer content={content} />
         : null}
 
-      {embedImages && embedImages.length > 0
-        && (
-        <>
-          <Button
-            size="sm"
-            variant="outline"
-            onClick={onEmbedToggle}
-            className="mt-3"
-          >
-            { showEmbed ? <EyeOffIcon /> : <EyeIcon /> }
-            { showEmbed ? 'Hide' : 'Show' } images
-          </Button>
-
-          {showEmbed && (
-            <div className="mt-2 flex gap-x-2">
-              {embedImages.map(image => (
-                <div
-                  className="max-h-[26rem]"
-                  style={{
-                    aspectRatio: image.aspectRatio ? image.aspectRatio.width / image.aspectRatio.height : undefined
-                  }}
-                >
-                  <img 
-                    src={image.thumb} 
-                    alt={image.alt}
-                    className="max-h-full max-w-full rounded-lg border object-cover"
-                    loading="lazy"
-                    width={image.aspectRatio?.width}
-                    height={image.aspectRatio?.height}
-                  />
-                </div>
-              ))}
-            </div>
-          )}
-        </>
+      {embedImages && embedImages.length > 0 && (
+        <EmbedToggle className="mt-3" label="images">
+          <div className="mt-2 flex gap-x-2">
+            {embedImages.map(image => (
+              <div
+                key={image.thumb}
+                className="max-h-[26rem] w-fit"
+                style={{
+                  aspectRatio: image.aspectRatio ? image.aspectRatio.width / image.aspectRatio.height : undefined
+                }}
+              >
+                <img 
+                  src={image.thumb} 
+                  alt={image.alt}
+                  className="max-h-full max-w-full rounded-lg border object-cover"
+                  loading="lazy"
+                  width={image.aspectRatio?.width}
+                  height={image.aspectRatio?.height}
+                />
+              </div>
+            ))}
+          </div>
+        </EmbedToggle>
       )}
 
       {embedVideo !== undefined ? (
-        <>
-          <Button
-            size="sm"
-            variant="outline"
-            onClick={onEmbedToggle}
-            className="mt-3"
+        <EmbedToggle className="mt-3" label="video">
+          <div
+            className="mt-2 max-h-[26rem] w-fit"
+            style={{
+              aspectRatio: embedVideo.aspectRatio ? embedVideo.aspectRatio.width / embedVideo.aspectRatio.height : undefined
+            }}
           >
-            { showEmbed ? <EyeOffIcon /> : <EyeIcon /> }
-            { showEmbed ? 'Hide' : 'Show' } video
-          </Button>
-
-          {showEmbed && (
-            <div
-              className="mt-2 max-h-[26rem]"
-              style={{
-                aspectRatio: embedVideo.aspectRatio ? embedVideo.aspectRatio.width / embedVideo.aspectRatio.height : undefined
-              }}
-            >
-              <HLSPlayer
-                autoPlay
-                className="max-h-full max-w-full rounded-lg border object-contain"
-                src={embedVideo.playlist}
-                width={embedVideo.aspectRatio?.width}
-                height={embedVideo.aspectRatio?.height}
-                poster={embedVideo.thumbnail}
-                onClick={(e) => e.stopPropagation()}
-              />
-            </div>
-          )}
-        </>
+            <HLSPlayer
+              autoPlay
+              className="max-h-full max-w-full rounded-lg border object-contain"
+              src={embedVideo.playlist}
+              width={embedVideo.aspectRatio?.width}
+              height={embedVideo.aspectRatio?.height}
+              poster={embedVideo.thumbnail}
+              onClick={(e) => e.stopPropagation()}
+            />
+          </div>
+        </EmbedToggle>
       ) : null}
 
       {embedExternal && (
-        gifUriRegex.test(embedExternal.uri)
-        ? (
-          <>
-            <Button
-              size="sm"
-              variant="outline"
-              onClick={onEmbedToggle}
-              className="mt-3"
-            >
-              { showEmbed ? <EyeOffIcon /> : <EyeIcon /> }
-              { showEmbed ? 'Hide' : 'Show' } GIF
-            </Button>
-
-            {showEmbed && (
-              <div className="max-h-[26rem] mt-2">
-                <img 
-                  src={embedExternal.uri} 
-                  alt={embedExternal.title}
-                  className="max-h-full max-w-full rounded-lg border object-cover"
-                  loading="lazy"
-                />
-              </div>
-            )}
-          </>
-        )
-        : ytUriRegex.test(embedExternal.uri)
-        ? (
-          <YouTubeEmbed
-            id={ytUriRegex.exec(embedExternal.uri)![1]}
-            title={embedExternal.title}
-            className="mt-3"
-            onClick={(e) => e.stopPropagation()}
-          />
-        )
-        : (
-          <div className="mt-3 bg-background border rounded-lg overflow-hidden relative transition-[scale] active:scale-[98%]">
-            {embedExternal.thumb && (
-              <div className="bg-secondary border-b">
-                <img
-                  src={embedExternal.thumb}
-                  width="1200"
-                  height="630"
-                  loading="lazy"
-                  className="max-h-full max-w-full aspect-[120/63] object-cover"
-                />
-              </div>
-            )}
-            <div className="flex flex-col gap-y-1 py-3 px-3">
-              <small className="text-muted-foreground flex items-center gap-x-1">
-                <GlobeIcon size="1em" />
-                {new URL(embedExternal.uri).hostname.replace('www.', '')}
-              </small>
-              <a
-                className="line-clamp-2 text-pretty before:absolute before:inset-0 before:block before:size-full"
-                href={embedExternal.uri}
-                rel="noopener noreferrer"
-                target="_blank"
-                onClick={(e) => e.stopPropagation()}
-              >
-                {embedExternal.title}
-              </a>
-              {embedExternal.description ? <small className="line-clamp-1">{embedExternal.description}</small> : null}
-            </div>
-          </div>
-        )
+        <EmbedExternal view={embedExternal} />
       )}
     </>
   );
