87f3e86612aa2bd1b78c37702e93865e9f487106
Vladimir Antonio F. C. <50648871+vladiantio@users.noreply.github.com>
2025-04-07 21:56:14 -0400

    refactor(ProfileHeader): create AuthorHeader component for improved user profile display

---
+++ src/components/profile/AuthorHeader.tsx
@@ -0,0 +1,50 @@
+import { FC } from 'react';
+import { ArrowLeft } from 'lucide-react';
+import { useRouter } from '@tanstack/react-router';
+import { User } from '@/types/ResponseSchema';
+import { Button } from '../ui/button';
+
+interface AuthorHeaderProps {
+  user: Partial<User>;
+  isCurrentUser?: boolean;
+}
+
+const AuthorHeader: FC<AuthorHeaderProps> = ({
+  user,
+  isCurrentUser = false,
+}) => {
+  const { history } = useRouter();
+
+  return (
+    <div className="sticky top-0 z-10 bg-background px-4 h-14 flex items-center justify-between gap-x-3">
+      <Button
+        variant="ghost"
+        className="rounded-full !p-2 -ml-1"
+        onClick={() => history.go(-1)}
+      >
+        <ArrowLeft className="size-5" />
+      </Button>
+      <div className="text-center text-sm overflow-hidden [&>*]:truncate">
+        {user.displayName && (
+          <p className="font-semibold">{user.displayName}</p>
+        )}
+        {user.username && (
+          <p className="text-muted-foreground">@{user.username}</p>
+        )}
+      </div>
+      <div className="flex items-center">
+        {!isCurrentUser ? (
+          <Button size="sm">
+            Follow
+          </Button>
+        ) : (
+          <Button size="sm" variant="outline">
+            Edit profile
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default AuthorHeader;
--- src/components/profile/ProfileHeader.tsx
+++ src/components/profile/ProfileHeader.tsx
@@ -1,10 +1,8 @@
 import { FC } from 'react';
-import { ArrowLeft } from 'lucide-react';
-import { useRouter } from '@tanstack/react-router';
 import { User } from '@/types/ResponseSchema';
-import { Button } from '../ui/button';
 import { ProfileDisplay } from './ProfileDisplay';
 import { ProfileTabs } from './ProfileTabs';
+import AuthorHeader from './AuthorHeader';
 
 interface ProfileHeaderProps {
   user: User;
@@ -12,34 +10,12 @@ interface ProfileHeaderProps {
 }
 
 const ProfileHeader: FC<ProfileHeaderProps> = ({ user, isCurrentUser }) => {
-  const { history } = useRouter();
-
   return (
     <>
-      <div className="sticky top-0 z-10 bg-background px-4 h-14 flex items-center justify-between gap-x-3">
-        <Button
-          variant="ghost"
-          className="rounded-full !p-2 -ml-1"
-          onClick={() => history.go(-1)}
-        >
-          <ArrowLeft className="size-5" />
-        </Button>
-        <div className="text-center text-sm overflow-hidden [&>*]:truncate">
-          <p className="font-bold">{user.displayName}</p>
-          <p className="text-muted-foreground">@{user.username}</p>
-        </div>
-        <div className="flex items-center">
-          {!isCurrentUser ? (
-            <Button size="sm">
-              Follow
-            </Button>
-          ) : (
-            <Button size="sm" variant="outline">
-              Edit profile
-            </Button>
-          )}
-        </div>
-      </div>
+      <AuthorHeader
+        user={user}
+        isCurrentUser={isCurrentUser}
+      />
 
       <div className="px-4 pb-4">
         {user.banner ? (
