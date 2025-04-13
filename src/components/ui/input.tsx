c58af4c9154354f0e6cc543d5b04e794766ccbac
Vladimir Antonio F. C. <50648871+vladiantio@users.noreply.github.com>
2025-04-13 00:10:21 -0400

    feat(Input): add InputGroup and InputGroupText components for enhanced input handling

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -9,6 +9,8 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
       data-slot="input"
       className={cn(
         "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        "group-has-[[data-slot=text]:first-child]/input:ps-9",
+        "group-has-[[data-slot=text]:last-child]/input:pe-9",
         "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
         "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
         className
@@ -18,4 +20,33 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
   )
 }
 
-export { Input }
+function InputGroup({ className, children, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      className={cn("group/input relative", className)}
+      {...props}
+    >
+      {children}
+    </div>
+  )
+}
+
+function InputGroupText({ className, children, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      className={cn(
+        "text-muted-foreground/80 pointer-events-none absolute inset-y-0 flex items-center justify-center",
+        "first:start-0 first:ps-3",
+        "last:end-0 last:pe-3",
+        "group-has-[input:focus]/input:text-primary group-has-[input:disabled]/input:opacity-50",
+        className
+      )}
+      data-slot="text"
+      {...props}
+    >
+      {children}
+    </div>
+  )
+}
+
+export { Input, InputGroup, InputGroupText }
