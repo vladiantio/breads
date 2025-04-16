45d7d8ed5b7a9e7613d2d79dab4b05e823137dd5
Vladimir Antonio F. C. <50648871+vladiantio@users.noreply.github.com>
2025-04-15 23:40:46 -0400

    feat(Routing): implement session restoration and redirection logic in root route

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,6 @@
-import { createRootRoute, Outlet } from '@tanstack/react-router'
+import { createRootRoute, Outlet, redirect } from '@tanstack/react-router'
 import { Toaster } from "@/components/ui/sonner"
+import { useAtpStore } from '@/lib/atp/store';
 // import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
 export const Route = createRootRoute({
@@ -10,4 +11,29 @@ export const Route = createRootRoute({
       {/* <TanStackRouterDevtools /> */}
     </>
   ),
+  beforeLoad: async ({ location }) => {
+    const {
+      restoreSession,
+      isAuthenticated,
+    } = useAtpStore.getState();
+
+    // Attempt to restore the session
+    await restoreSession();
+
+    if (location.pathname.startsWith('/login')) {
+      // if already authenticated, redirect to root
+      if (isAuthenticated) throw redirect({ to: '/' });
+
+      // if not authenticated, proceed to login
+      return;
+    }
+
+    // redirect /@username to /profile/username
+    if (location.pathname.startsWith('/@')) {
+      throw redirect({
+        to: '/profile/$username',
+        params: { username: location.pathname.slice(2) },
+      });
+    }
+  },
 });
