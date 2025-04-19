68c3c25af5fada8dd4276407412c10637d791176
Vladimir Antonio F. C. <50648871+vladiantio@users.noreply.github.com>
2025-04-19 01:30:18 -0400

    refactor(PostCardHeader): replace AuthorHoverCard with AuthorLink for improved readability

--- src/components/feed/PostCardHeader.tsx
+++ src/components/feed/PostCardHeader.tsx
@@ -1,8 +1,7 @@
-import { Link } from '@tanstack/react-router';
 import { User } from '@/data/users';
 import { formatTimestamp } from '@/utils/date';
 import { PostCardMenu, PostCardMenuProps } from './PostCardMenu';
-import { AuthorHoverCard } from './AuthorHoverCard';
+import { AuthorLink } from '../shared/AuthorLink';
 
 interface PostCardHeaderProps extends PostCardMenuProps {
   author: Partial<User>;
@@ -17,18 +16,7 @@ const PostCardHeader: React.FC<PostCardHeaderProps> = ({
   return (
     <div className="flex items-center justify-between gap-x-2">
       <div className="flex items-center gap-x-2 overflow-hidden text-muted-foreground">
-        <AuthorHoverCard handle={author.username}>
-          <Link
-            to="/profile/$username"
-            params={{
-              username: author.username!,
-            }}
-            className="font-semibold text-foreground hover:underline active:opacity-60 truncate"
-            onClick={(e) => e.stopPropagation()}
-          >
-            {author.username}
-          </Link>
-        </AuthorHoverCard>
+        <AuthorLink username={author.username!} />
         <span>·</span>
         <time dateTime={timestamp} className="shrink-0">{formatTimestamp(timestamp)}</time>
       </div>
