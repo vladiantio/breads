638af62a6441b5aa961fc834cf4d4f9c46ab0884
Vladimir Antonio F. C. <50648871+vladiantio@users.noreply.github.com>
2025-04-19 01:36:02 -0400

    feat(Post): add useDocumentTitle for dynamic page title in post view

--- src/routes/_app.profile.$username.post.$postId.tsx
+++ src/routes/_app.profile.$username.post.$postId.tsx
@@ -1,5 +1,6 @@
 import PostCard from '@/components/feed/PostCard';
 import AuthorHeader from '@/components/profile/AuthorHeader';
+import { useDocumentTitle } from '@/hooks/use-document-title';
 import { usePostThread } from '@/lib/atp/hooks/use-post-thread';
 import { createFileRoute } from '@tanstack/react-router'
 
@@ -15,6 +16,10 @@ function RouteComponent() {
     data,
     isLoading,
   } = usePostThread({ uri });
+
+  useDocumentTitle(data?.post && (
+    data.post.content || `${data.post.embedImages ? `Image${data.post.embedImages.length > 1 ? 's' : ''}` : data.post.embedVideo ? 'Video' : 'Media'} posted by ${(data.post.author.displayName ? `${data.post.author.displayName} (@${data.post.author.username})` : `@${data.post.author.username}`)}`
+  ));
 
   if (isLoading)
     return "Loading...";
