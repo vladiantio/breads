ad7d19508ab4073a493b967e65279d04a5ea30ff
Vladimir Antonio F. C. <50648871+vladiantio@users.noreply.github.com>
2025-04-21 01:48:45 -0400

    feat(main): import polyfill

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './styles/index.css'
+import './lib/polyfill'
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
