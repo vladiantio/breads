f342a29eec66ba069b2378cb16fcae0e3422551b
Vladimir Antonio F. C. <50648871+vladiantio@users.noreply.github.com>
2025-05-12 00:40:38 -0400

    feat(Theme): enhance theme management with theme settings and styles application

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,6 +1,8 @@
+import { ThemePresetSelect } from "@/components/settings/ThemePresetSelect";
 import { Button } from "@/components/ui/button";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { useTheme } from "@/theme/ThemeProvider";
+import { presets } from "@/utils/theme-presets";
 import { useRouter } from "@tanstack/react-router";
 import { ArrowLeft, Moon, Sun, SunMoon } from "lucide-react";
 
@@ -12,7 +14,16 @@ const themeOptions = [
 
 export function Settings() {
   const { history } = useRouter();
-  const { theme, setTheme } = useTheme();
+  const { theme, themeSettings, setTheme, setThemeSettings } = useTheme();
+
+  const handlePresetChange = (preset: string) => {
+    setThemeSettings({
+      theme: {
+        preset,
+        styles: presets[preset],
+      }
+    })
+  }
 
   return (
     <>
@@ -28,30 +39,40 @@ export function Settings() {
           Settings
         </div>
       </div>
-      <div className="p-4">
-        <p className="mb-1">Color mode</p>
-        <RadioGroup
-          className="grid grid-cols-3 gap-2"
-          value={theme}
-          onValueChange={setTheme}
-        >
-          {themeOptions.map((option) => (
-            <label
-              key={option.value}
-              className="border-input has-data-[state=checked]:border-primary/50 has-focus-visible:border-ring has-focus-visible:ring-ring/50 relative flex cursor-pointer flex-col items-center gap-3 rounded-md border px-2 py-3 text-center shadow-xs transition-[color,box-shadow] outline-none has-focus-visible:ring-[3px] has-data-disabled:cursor-not-allowed has-data-disabled:opacity-50"
-            >
-              <RadioGroupItem
-                id={option.value}
-                value={option.value}
-                className="sr-only after:absolute after:inset-0"
-              />
-              <p className="text-foreground text-sm leading-none font-medium inline-flex items-center gap-x-2">
-                <option.icon className="inline-block size-4" />
-                {option.label}
-              </p>
-            </label>
-          ))}
-        </RadioGroup>
+      <div className="p-4 space-y-4">
+        <div>
+          <p className="mb-1">Color mode</p>
+          <RadioGroup
+            className="grid grid-cols-3 gap-2"
+            value={theme}
+            onValueChange={setTheme}
+          >
+            {themeOptions.map((option) => (
+              <label
+                key={option.value}
+                className="border-input has-data-[state=checked]:border-primary/50 has-focus-visible:border-ring has-focus-visible:ring-ring/50 relative flex cursor-pointer flex-col items-center gap-3 rounded-md border px-2 py-3 text-center shadow-xs transition-[color,box-shadow] outline-none has-focus-visible:ring-[3px] has-data-disabled:cursor-not-allowed has-data-disabled:opacity-50"
+              >
+                <RadioGroupItem
+                  id={option.value}
+                  value={option.value}
+                  className="sr-only after:absolute after:inset-0"
+                />
+                <p className="text-foreground text-sm leading-none font-medium inline-flex items-center gap-x-2">
+                  <option.icon className="inline-block size-4" />
+                  {option.label}
+                </p>
+              </label>
+            ))}
+          </RadioGroup>
+        </div>
+        <div>
+          <p className="mb-1">Theme Preset</p>
+          <ThemePresetSelect
+            presets={presets}
+            currentPreset={themeSettings?.theme?.preset}
+            onPresetChange={handlePresetChange}
+          />
+        </div>
       </div>
     </>
   );
--- src/theme/ThemeProvider.tsx
+++ src/theme/ThemeProvider.tsx
@@ -1,33 +1,81 @@
 import { createContext, useContext, useEffect, useState } from "react"
 
+import type { ThemeSettings, ThemeStyles } from '@/types/theme'
+
+import { COMMON_STYLES, defaultThemeState } from '@/config/theme'
+import { colorFormatter } from "@/utils/color-converter"
+
 type Theme = "dark" | "light" | "system"
 
 type ThemeProviderProps = {
   children: React.ReactNode
   defaultTheme?: Theme
   storageKey?: string
+  storageSettingsKey?: string
 }
 
 type ThemeProviderState = {
   theme: Theme
+  themeSettings: ThemeSettings
   setTheme: (theme: Theme) => void
+  setThemeSettings: (themeSettings: ThemeSettings) => void
 }
 
 const initialState: ThemeProviderState = {
   theme: "system",
+  themeSettings: {
+    theme: {
+      preset: null,
+      styles: null
+    }
+  },
   setTheme: () => null,
+  setThemeSettings: () => null,
 }
 
 const ThemeProviderContext = createContext<ThemeProviderState>(initialState)
+
+const applyThemeStyles = (root: HTMLElement, themeStyles: ThemeStyles) => {
+  Object.entries(themeStyles.light)
+    .filter(([key, value]) => COMMON_STYLES.includes(key as (typeof COMMON_STYLES)[number]) && typeof value === 'string')
+    .forEach(([key, value]) => {
+      root.style.setProperty(`--${key}`, value)
+    })
+
+  Object.entries(themeStyles.light)
+    .filter(([key, value]) => !COMMON_STYLES.includes(key as (typeof COMMON_STYLES)[number]) && typeof value === 'string')
+    .forEach(([key, value]) => {
+      root.style.setProperty(`--${key}--light`, colorFormatter(value, 'oklch'))
+    })
+
+  Object.entries(themeStyles.dark)
+    .filter(([key, value]) => !COMMON_STYLES.includes(key as (typeof COMMON_STYLES)[number]) && typeof value === 'string')
+    .forEach(([key, value]) => {
+      root.style.setProperty(`--${key}--dark`, colorFormatter(value, 'oklch'))
+    })
+}
+
+const initialThemeSettings: ThemeSettings = {
+  theme: {
+    preset: null,
+    styles: null
+  }
+}
 
 function ThemeProvider({
   children,
   defaultTheme = "system",
   storageKey = "theme",
+  storageSettingsKey = "theme-settings",
   ...props
 }: ThemeProviderProps) {
   const [theme, setTheme] = useState<Theme>(
     () => (localStorage.getItem(storageKey) as Theme) || defaultTheme
+  )
+  const [themeSettings, setThemeSettings] = useState<ThemeSettings>(
+    () => (typeof localStorage.getItem(storageSettingsKey) === 'string'
+      ? JSON.parse(localStorage.getItem(storageSettingsKey) as string) as ThemeSettings
+      : initialThemeSettings)
   )
 
   useEffect(() => {
@@ -48,11 +96,36 @@ function ThemeProvider({
     root.classList.add(theme)
   }, [theme])
 
+  useEffect(() => {
+    const root = window.document.documentElement
+
+    if (themeSettings.theme.styles) {
+      const styles = {
+        light: {
+          ...defaultThemeState.light,
+          ...themeSettings.theme.styles.light,
+        },
+        dark: {
+          ...defaultThemeState.dark,
+          ...themeSettings.theme.styles.dark,
+        }
+      }
+      applyThemeStyles(root, styles)
+    } else {
+      root.removeAttribute('style')
+    }
+  }, [themeSettings])
+
   const value = {
     theme,
+    themeSettings,
     setTheme: (theme: Theme) => {
       localStorage.setItem(storageKey, theme)
       setTheme(theme)
+    },
+    setThemeSettings: (themeSettings: ThemeSettings) => {
+      localStorage.setItem(storageSettingsKey, JSON.stringify(themeSettings))
+      setThemeSettings(themeSettings)
     },
   }
 
