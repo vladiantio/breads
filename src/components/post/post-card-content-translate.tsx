5711f1ab6c3818f063405d92a31028107d1da446
Vladimir Antonio F. C. <50648871+vladiantio@users.noreply.github.com>
2025-08-05 10:34:56 -0400

    fix(PostCardContentTranslate): reset error state before starting translation process

--- src/components/post/post-card-content-translate.tsx
+++ src/components/post/post-card-content-translate.tsx
@@ -33,6 +33,7 @@ export function PostCardContentTranslate() {
       return
     }
 
+    setError("")
     setIsTranslating(true)
     try {
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
