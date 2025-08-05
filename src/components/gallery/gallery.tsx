f6763da0a2c174f7e213f67f4b774ef81b63244a
Vladimir Antonio F. C. <50648871+vladiantio@users.noreply.github.com>
2025-08-05 12:46:45 -0400

    feat(MediaCard): add replies count display

--- src/components/gallery/gallery.tsx
+++ src/components/gallery/gallery.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { PostWithAuthor } from "@/types/response-schema"
 import { Link } from "@tanstack/react-router"
-import { AlertCircleIcon, FilmIcon, HeartIcon, ImageIcon, ImagesIcon, RepeatIcon, Volume2Icon, VolumeOffIcon } from "lucide-react"
+import { AlertCircleIcon, FilmIcon, HeartIcon, ImageIcon, ImagesIcon, MessageCircleIcon, RepeatIcon, Volume2Icon, VolumeOffIcon } from "lucide-react"
 import { HLSPlayer } from "../shared/hls-player"
 import { isMobileDevice } from "@/lib/browser"
 import { MasonryVerticalVirtualizerDynamic } from "@/ui/virtualizer"
@@ -83,14 +83,17 @@ function MediaCard({ post }: { post: PostWithAuthor }) {
                 <ImageIcon />
               )}
             </div>
-            {(post.likes > 0 || post.reposts > 0) && (
+            {(post.likes > 0 || post.replies > 0 || post.reposts > 0) && (
               <div className="dark absolute bottom-2 left-2 bg-background/50 backdrop-blur-sm text-foreground px-3 py-2 rounded-full [&_svg]:size-4 flex gap-2 font-semibold text-xs">
-              {post.likes > 0 && (
-                <div className="flex items-center gap-1"><HeartIcon /><span>{formatNumber(post.likes)}</span></div>
-              )}
-              {post.reposts > 0 && (
-                <div className="flex items-center gap-1"><RepeatIcon /><span>{formatNumber(post.reposts)}</span></div>
-              )}
+                {post.likes > 0 && (
+                  <div className="flex items-center gap-1"><HeartIcon /><span>{formatNumber(post.likes)}</span></div>
+                )}
+                {post.replies > 0 && (
+                  <div className="flex items-center gap-1"><MessageCircleIcon /><span>{formatNumber(post.replies)}</span></div>
+                )}
+                {post.reposts > 0 && (
+                  <div className="flex items-center gap-1"><RepeatIcon /><span>{formatNumber(post.reposts)}</span></div>
+                )}
               </div>
             )}
           </>
