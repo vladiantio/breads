f7fc666670786a283186eb83dda8e7dd369d1882
Vladimir Antonio F. C. <50648871+vladiantio@users.noreply.github.com>
2025-10-22 13:43:41 -0300

    refactor(vite): update TanStack Router import for consistency in configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,17 @@ import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import svgr from 'vite-plugin-svgr'
 import { lingui } from "@lingui/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
     react({ plugins: [["@lingui/swc-plugin", {}]] }),
     lingui(),
     tailwindcss(),
